93cfdaac9c6125db012e8f543acdcb671a197dcd
jaywu12375 <37837961+jaywu12375@users.noreply.github.com>
2018-04-29 15:25:02 -0400

    updated

--- sketch.js
+++ sketch.js
@@ -1,22 +1,44 @@
+
 // A HTML range slider
 var slider;
-
+var p = 0; //count of points
+var numOfpoints = 5; //how many points we want
 
 function setup() {
-  createCanvas(700, 700, WEBGL);
+  createCanvas(600, 600);
+  // hue, saturation, and brightness
   colorMode(HSB, 255);
   // slider has a range between 0 and 255 with a starting value of 127
   slider = createSlider(0, 255, 127);
+  slider.position (450, 570)
 }
 
 function draw() {
-  background(50);
-  //object rotates with mouse
-  rotateY(map(mouseX, 0, width, 0, PI));
-  rotateX(map(mouseY, 0, height, 0, PI));
+  background(0);
+  strokeWeight(3);
+
+  //text
+  textSize(15);
+  textAlign(RIGHT);
+  textFont("Helvetica");
+  noStroke()
+  fill(slider.value(), 255, 255, 127); //text match slider color
+  text("Color Slider",530, 565);
+
   // Set the hue according to the slider
-  strokeWeight (2)
   stroke(slider.value(), 255, 255);
   fill(slider.value(), 255, 255, 127);
-  box(200);
+  if (mouseIsPressed) { //changes the shape if mouse is pressed
+    rect(150, 150, 300, 300)
+  } else {
+    ellipse(300, 300, 300, 300);
+  }
+
+  
+  //sparkly sky (points are redrawn randomly on the canvas everytime we go through the draw function
+  stroke(slider.value(), 255, 255);
+  //loop for 5 points 
+  for (p = 0; p < numOfpoints; p ++){
+    point(random(0,width),random(0,height));
+  }
 }
